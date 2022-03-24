4a4f445764cc96851055e17cd73d7d4c93a214d0
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-03-23 21:34:04 -0300

    Initial commit

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,13 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User';
+
+export default class UsersController {
+  public async create({ request }: HttpContextContract) {
+    const { name, username } = request.only(["name", "username"]);
+    const user = await User.create({
+      name,
+      username
+    })
+    return user;
+  }
+}
